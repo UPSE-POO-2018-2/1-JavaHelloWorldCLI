07c0a74e213beff46be047bb7da479cd872009d3
ivansanchezvera <ivansanchez_1988@hotmail.com>
2018-10-17 11:01:48 -0500

    Code: Primer programa en Java. Contiene un main como punto de entrada para la ejecucion del programa. El main solo contiene salidas de consola, imprimiendo mensajes.

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Que hubo UPSE! Caterva!");
+		System.out.println("Saludos desde Sublime!");
+		System.out.println("Sin compilar, es como sin pijama!");
+		
+		System.out.println("Probando compilacion desde CLI.");
+
+	}
+
+}
